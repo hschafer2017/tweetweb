db983ba1ff41d85e75b7ea08d2fe2702c75217fa
Haley Schafer <hschafer@umich.edu>
2018-06-05 17:42:38 +0000

    Storing Tweet Searches to MongoDB

--- mongo_store.py
+++ mongo_store.py
@@ -1,32 +1,14 @@
-import os
-from tweets import search
-from flask import Flask, redirect, render_template, request
 from tweepy import Stream
 from tweepy.streaming import StreamListener
 from auth import get_auth
 from pymongo import MongoClient
 import json
-import json
-import tweepy
-from tweepy import OAuthHandler
 
+MONGODB_URI = "mongodb://brian-haley:pa55word@ds247310.mlab.com:47310/bglynch-twitter"
 
-MONGODB_URI = ""
-MONGODB_NAME = ""
+MONGODB_NAME = "bglynch-twitter"
 
-app = Flask(__name__)
-
-@app.route('/')
-def get_homepage():
-   return render_template("index.html")
-
-@app.route('/new_search')
-def get_results_page():
-    q = request.args.get('search')
-    tweets = search(q, 10)
-    
-#Insert to Mongo
-
+def mongo_store(q):
     class MyStreamListener(StreamListener):
         # Class that Tweepy has built, has collects all functionality of tweets
     
@@ -52,15 +34,6 @@ def get_results_page():
         def on_error(self, status):
             print(status)
             return True
-    
-    auth = get_auth()
-    
-    twitter_stream = Stream(auth, MyStreamListener())
-    twitter_stream.filter(track=q)
-
-
-    return render_template("results.html", tweets = tweets, q = q)
-
-
-if __name__ == '__main__':
-    app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug = True)
+        auth = get_auth()
+        twitter_stream = Stream(auth, MyStreamListener())
+        twitter_stream.filter(track=q)
--- tweets.py
+++ tweets.py
@@ -5,9 +5,4 @@ api = get_api()
 
 
 def search(query, count):
-    return [{'text':status.text, 'id':status.id} for status in tweepy.Cursor(api.search, q=query).items(count)]
-
-# code for testing    
-# tweets = search("Trump", 10)
-# for tweet in tweets: 
-#     print(tweet)
+    return [status for status in tweepy.Cursor(api.search, q=query).items(count)]
